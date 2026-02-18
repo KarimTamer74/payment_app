1f225a758c3b84387de00065cc7420cd2371dc67
KarimTamer74 <kemotamer448@gmail.com>
2026-02-18 13:43:07 +0200

    intgrate payment with paypal done

--- lib/features/checkout/presentation/widgets/complete_payment_button.dart
+++ lib/features/checkout/presentation/widgets/complete_payment_button.dart
@@ -3,9 +3,14 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payment_app/core/shared_widgets/custom_button.dart';
+import 'package:payment_app/core/utils/api_keys.dart';
 import 'package:payment_app/core/utils/constants.dart';
-import 'package:payment_app/features/checkout/data/models/payment_intent_request.dart';
+import 'package:payment_app/features/checkout/data/models/paypal_models/amount_model.dart';
+import 'package:payment_app/features/checkout/data/models/paypal_models/item_list_model.dart';
+import 'package:payment_app/features/checkout/data/models/paypal_models/transactions_model.dart';
+import 'package:payment_app/features/checkout/data/models/stripe_models/payment_intent_request.dart';
 import 'package:payment_app/features/checkout/presentation/cubits/stripe_payment_cubit/stripe_payment_cubit.dart';
 import 'package:payment_app/features/checkout/presentation/cubits/stripe_payment_cubit/stripe_payment_states.dart';
 
@@ -32,6 +37,7 @@ class CompletePaymentButton extends StatelessWidget {
                   ? 'Please wait...'
                   : 'Complete Payment',
           onPressed: () {
+            log('Current Index: $currentIndex', name: 'CompletePaymentButton');
             if (currentIndex == 0) {
               BlocProvider.of<StripePaymentCubit>(context).makePayment(
                 paymentIntentInput: PaymentIntentRequest(
@@ -41,11 +47,58 @@ class CompletePaymentButton extends StatelessWidget {
               );
               // Navigator.pushNamed(context, payWithCard);
             } else if (currentIndex == 1) {
-              Navigator.pushNamed(context, paymentSuccess);
+              makePaypalPaymentProccess(context);
             }
           },
         );
       },
     );
+  }
+
+  void makePaypalPaymentProccess(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder:
+            (BuildContext context) => PaypalCheckoutView(
+              sandboxMode: true,
+              clientId: ApiKeys.paypalClientId,
+              secretKey: ApiKeys.paypalSecretKey,
+              transactions: [getOrderDetails()],
+              note: "Contact us for any questions on your order.",
+              onSuccess: (Map params) async {
+                log("onSuccess: $params");
+                Navigator.pop(context);
+                Navigator.pushReplacementNamed(context, paymentSuccess);
+              },
+              onError: (error) {
+                log("onError: $error");
+                Navigator.pop(context);
+              },
+              onCancel: () {
+                log('cancelled:');
+                Navigator.pop(context);
+              },
+            ),
+      ),
+    );
+  }
+
+  Map getOrderDetails() {
+    var details = DetailsModel(
+      subtotal: '100',
+      shipping: '0',
+      shippingDiscount: 0,
+    );
+    var amount = AmountModel(total: "100", currency: "USD", details: details);
+    List<ItemModel> items = [
+      ItemModel(name: "Apple", quantity: 4, price: "10", currency: "USD"),
+      ItemModel(name: "Pineapple", quantity: 5, price: "12", currency: "USD"),
+    ];
+    var transactionsModel = TransactionsModel(
+      amount: amount,
+      description: 'The payment transaction description.',
+      itemList: ItemListModel(items: items),
+    );
+    return transactionsModel.toMap();
   }
 }
